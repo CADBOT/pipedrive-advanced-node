391a60b22222b6ae392e15838a09991bc02f5b2c
Charles Crawford <charliec364@gmail.com>
2016-09-23 00:30:58 -0700

    update

--- 1-buffers/buffers-intro.js
+++ 1-buffers/buffers-intro.js
@@ -110,13 +110,13 @@ buff = Buffer.from('dXNlcm5hbwU6cGFzc3dvcmQ=', 'base64')
 
 buff = Buffer.alloc(2)
 buff.writeUInt16BE(18505) // Big Endian (we'll discuss that shortly)
-console.log(buff)
-console.log(buff.toString())
+//console.log(buff)
+//console.log(buff.toString())
 // same thing as if we did
 buff.writeUInt8(72) // 01001000 => 72(decimal)
 buff.writeUInt8(73, 1)
-console.log(buff)
-console.log(buff.toString())
+//console.log(buff)
+//console.log(buff.toString())
 
 /*
  *
@@ -126,19 +126,57 @@ console.log(buff.toString())
  * Unsigned 16 big Big Endian integers (if possible)
  */
 
+// Big Endian vs Little Endian
+//
+// the order in which we store the bytes
+//
+// Big Endian the Most sig byte comes first(human intuitive)
+//
+// 48, 49, 50
+// 1st byte  2nd byte  3rd byte
+// 01001000  01001001  01001010
+//
+// In little endian
+// 01001010  01001001  01001000  
+//
+// Observe that within each byte. The order
+// is perserved
+//
 
+buff = Buffer.alloc(2)
+buff.writeUInt16LE(18505)
+//console.log(buff)
+//console.log(buff.toString())
 
+/*
+ * I like to stick to Big Endian unless I'm doing low level programming
+ * related to Little Endian
+ */
 
+// we can slice up buffers. Does NOT do a copy or clone. It's another ref
+// to the same bytes
 
+buff = Buffer.from('Hello world!')
+var view = buff.slice(6, 11) // inclusive start index. Exclusive end index
+//console.log(view.toString())
 
+view.write('bye')
+console.log(view.toString())
+// our original buffer is modified as well
+//console.log(buff.toString())
+view.write('byeeeeeeeeee')
 
+// We can read by various set amounts
+buff = Buffer.from('Hello world!')
+console.log(buff.readUInt8(0))
+console.log(buff.readUInt8(1))
+// isn't very useful, as [] notation is kinda sorta syntatic sugar for readUInt8 and writeUInt8
+console.log(buff[0])
+console.log(buff[1])
 
-
-
-
-
-
-
+// these get useful when we want to bring in more than a byte, but less than the entire file
+var c = buff.readUInt16BE()
+console.log(c)
 
 
 
