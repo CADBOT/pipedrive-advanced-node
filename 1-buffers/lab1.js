2dc08460350b3f957888e08baeefa3f3c7a49046
Charles Crawford <charliec364@gmail.com>
2016-09-23 00:38:02 -0700

    add lab1 instructions

---
+++ 1-buffers/lab1.js
@@ -0,0 +1,16 @@
+/*
+ * @day is a string that represents a day of the week ('Mon' 'Tue'
+ * @record is a buffer of size 1 (just 1 byte). That represents that employees
+ * attendece
+ */
+function attendance(day, record) {
+
+}
+
+/*
+* @record is a buffer of size 1 (just 1 byte). That represents that employees
+* i.e. if record was equal 3  0000 0011
+* Days attended: Monday, Tuesday
+*/
+function printDaysAttended(record) {
+}
